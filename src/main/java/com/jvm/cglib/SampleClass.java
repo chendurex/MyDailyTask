c6be9a8997837c77eb52a7f0c33084132c05b920
chendurex <chendurex@gmail.com>
2016-05-26 14:15:01 +0800

    gradle doc

--- src/main/java/com/jvm/cglib/SampleClass.java
+++ src/main/java/com/jvm/cglib/SampleClass.java
@@ -7,7 +7,13 @@ package com.jvm.cglib;
  * @date 2016/5/25 9:13
  */
 public class SampleClass {
+    private String name;
+
     public String test(String input) {
-        return "Hello world!";
+        return "hi:" + name + " ,Hello world!" + input;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
