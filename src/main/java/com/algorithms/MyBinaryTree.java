cac80d9ddf06e5742188b177dc1fe81f04a538cc
chendurex <chendurex@gmail.com>
2016-07-23 23:50:50 +0800

    binary tree

--- src/main/java/com/algorithms/MyBinaryTree.java
+++ src/main/java/com/algorithms/MyBinaryTree.java
@@ -6,46 +6,115 @@ package com.algorithms;
 public class MyBinaryTree<E extends Comparable> {
 
     public static void main(String[] args) {
-        MyBinaryTree tree = new MyBinaryTree(10);
-        tree.insert(5);
-        tree.insert(20);
+        MyBinaryTree tree = new MyBinaryTree(5);
+        tree.insert(1);tree.insert(10);tree.insert(15);tree.insert(20);
+        tree.insert(25);tree.insert(18);tree.insert(8);tree.insert(21);
+        tree.insert(29);tree.insert(31);tree.insert(11);tree.insert(17);
+        System.out.println(tree.preNode(tree.get(21)).ele);
+        System.out.println(tree.nextNode(tree.get(20)).ele);
     }
 
     private Node root;
 
     public MyBinaryTree(E ele) {
-        root = new Node(null, ele, null);
+        root = new Node(null, null, ele, null);
     }
+
+    public Node preNode(Node node) {
+        if (node.left != null) {
+            if (node.left.right != null) {
+                Node preNode = node.left.right;
+                while (preNode.right != null) {
+                    preNode = preNode.right;
+                }
+                return preNode;
+            }
+            return node.left;
+        } else {
+            while (node.parent != null) {
+                if (node.parent.right == node) {
+                    return node.parent;
+                }
+                node = node.parent;
+            }
+        }
+        return new Node(null,null,null,null);
+    }
+
+    public Node nextNode(Node node) {
+        if (node.right != null) {
+            Node nextNode = node.right;
+            while (nextNode.left != null) {
+                nextNode = nextNode.left;
+            }
+            return nextNode;
+        } else {
+            Node nextNode = node;
+            while (nextNode.parent.left != nextNode) {
+                nextNode = nextNode.parent;
+            }
+            return nextNode.parent;
+        }
+    }
+
+    public Node get(E ele) {
+        if (ele == null) {
+            throw new IllegalArgumentException("value can not be null");
+        }
+        return get(ele, root);
+    }
+
+    private Node get(E ele, Node node) {
+        while (node != null) {
+            final int compare = ele.compareTo(node.ele);
+            if (compare == 0) {
+                return node;
+            } else if (compare < 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        return new Node(null, null, null, null);
+    }
+
 
     public void insert(E ele) {
         if (ele == null) {
             throw new IllegalArgumentException("value can not be null");
         }
-        insert(root,new Node(null,ele,null));
+        insert(new Node(null, null,ele,null), root);
     }
 
-    public void insert(Node node, Node another) {
-        if (node.ele.compareTo(another.ele) > 0) {
-            if (node.left == null) {
-                node.left = another;
+    private void insert(Node node, Node pNode) {
+        Node<E> parent = pNode;
+        while (pNode != null) {
+            parent = pNode;
+            final int compare = node.ele.compareTo(pNode.ele);
+            if (compare == 0) {
+                return ;
+            } else if (compare < 0) {
+                pNode = pNode.left;
             } else {
-                insert(node.left,another);
-            }
-        } else {
-            if (node.right == null) {
-                node.right = another;
-            } else {
-                insert(node.right,another);
+                pNode = pNode.right;
             }
         }
+        if (node.ele.compareTo(parent.ele) < 0) {
+            parent.left = node;
+        } else {
+            parent.right = node;
+        }
+        node.parent = parent;
     }
 
     private static class Node<E extends Comparable> {
         Node left;
         Node right;
+        Node parent;
         E ele;
 
-        public Node(Node left, E ele, Node right) {
+        public Node(Node parent,Node left, E ele, Node right) {
+            this.parent = parent;
             this.left = left;
             this.right = right;
             this.ele = ele;
