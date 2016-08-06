c4ee82f85bb195e43d943a183266ce9fba8d465e
chendurex <chendurex@gmail.com>
2016-08-06 14:25:44 +0800

    optimize fork join sort

--- src/main/java/com/algorithms/ForkJoinSort.java
+++ src/main/java/com/algorithms/ForkJoinSort.java
@@ -1,114 +1,65 @@
 package com.algorithms;
 
-import org.junit.Test;
-
 import java.util.Arrays;
 
 /**
  * Created by Administrator on 2016/6/28.
  */
 public class ForkJoinSort {
-    private int[] array = {1, 9, 18, 25, 4, 6, 2, 99, 50};
+    private static int[] array = {1, 9, 18, 25, 4, 6, 2, 99, 50};
 
-    @Test
-    public void forkJoinSort() {
-        //System.out.println(Arrays.toString(mergeArray(new int[]{1,3,7,8,20,23},new int[]{2,5,9,10,19})));
-        System.out.println(Arrays.toString(newArray(array)));
+    private static int[] originArray;
+    private static int[] compareArray;
+
+    public static void main(String[] args) {
+        sort(array);
+        System.out.println(Arrays.toString(array));
     }
 
-    private int[] newArray(int[] array) {
-        if (array.length == 1) {
-            return array;
-        } else if (array.length == 2) {
-            return baseSort(array[0], array[1]);
-        } else {
-            int middleLen = array.length >> 1;
-            int array1Len = middleLen;
-            int array2Len = array.length - middleLen;
-            int[] array1 = new int[array1Len];
-            int[] array2 = new int[array2Len];
-            for (int i = 0; i < array1Len; i++) {
-                array1[i] = array[i];
-            }
-            for (int k = 0; k < array2Len; k++) {
-                array2[k] = array[k + middleLen];
-            }
-            return mergeArray(newArray(array1), newArray(array2));
+
+    public static void sort(int[] array) {
+        originArray = array;
+        int len = originArray.length;
+        // according to variant either/or:
+        compareArray = new int[(len + 1) / 2];
+        mergeSort(0, len - 1);
+    }
+
+    private static void mergeSort(int offset, int limit) {
+        if (offset < limit) {
+            int middle = (offset + limit) / 2;
+            mergeSort(offset, middle);
+            mergeSort(middle + 1, limit);
+            merge(offset, middle, limit);
         }
     }
 
-    private int[] baseSort(int i, int k) {
-        if (i < k) {
-            return new int[]{i, k};
-        } else {
-            return new int[]{k, i};
+    private static void merge(int offset, int middle, int limit) {
+        int originIndex = offset;
+        int compareIndex = 0;
+        // 将原始数组的offset到middle间的元素复制到将要比较的数组
+        while (originIndex <= middle) {
+            compareArray[compareIndex++] = originArray[originIndex++];
         }
-    }
-
-
-    private int[] mergeArray(int[] array1, int[] array2) {
-        int array1Len = array1.length;
-        int array2Len = array2.length;
-        int mergeArrayLen = array1Len + array2Len;
-        int[] mergeArray = new int[mergeArrayLen];
-        for (int mergeIndex = 0, array1Index = 0, array2Index = 0; mergeIndex < mergeArrayLen; mergeIndex++) {
-            if (array1Index == array1Len) {
-                while (array2Index < array2Len) {
-                    mergeArray[mergeIndex] = array2[array2Index];
-                    array2Index++;
-                    mergeIndex++;
-                }
-            } else if (array2Index == array2Len) {
-                while (array1Index < array1Len) {
-                    mergeArray[mergeIndex] = array1[array1Index];
-                    array1Index++;
-                    mergeIndex++;
-                }
-            } else if (array1[array1Index] > array2[array2Index]) {
-                mergeArray[mergeIndex] = array2[array2Index];
-                array2Index++;
+        compareIndex = 0;
+        // 再次定义新的offset元素起始值，用于记录当前有多少元素已经排序
+        int originResetIndex = offset;
+        // 将新的数组与原数组比较排序
+        // originArray like  1 15 29 2 15 30
+        // compareArray like 1 15 29
+        // 这里因为compareArray是originArray复制过来的一份副本，所以如果compareArray比较小则不需要替换，直接下标++即可
+        while (originResetIndex < originIndex && originIndex <= limit) {
+            if (compareArray[compareIndex] > originArray[originIndex]) {
+                originArray[originResetIndex++] = originArray[originIndex++];
             } else {
-                mergeArray[mergeIndex] = array1[array1Index];
-                array1Index++;
+                //originArray[originResetIndex++] = compareArray[compareIndex++];
+                originResetIndex++;
+                compareIndex++;
             }
         }
-        return mergeArray;
-    }
-
-
-
-    public void sortArray(int array[], int offset, int position, int finish) {
-        int leftLen = position - offset;
-        int rightLen = finish - position;
-        int[] leftArray = new int[leftLen];
-        int[] rightArray = new int[rightLen];
-        for (int i = 0; i < leftLen; i++) {
-            leftArray[i] = array[offset + i];
-        }
-        for (int j = 0; j < rightLen; j++) {
-            rightArray[j] = array[position + j];
-        }
-        for (int k = offset,l = 0, r = 0; k < finish ;k ++) {
-            if (r == rightLen) {
-                while (l < leftLen) {
-                    array[k] = leftArray[l];
-                    l ++;
-                    k ++;
-                }
-            } else if (l == leftLen) {
-                while (r < rightLen) {
-                    array[k] = rightArray[r];
-                    r ++;
-                    k ++;
-                }
-            }else if (leftArray[l] > rightArray[r]) {
-                array[k] = rightArray[r];
-                r ++;
-            } else {
-                array[k] = leftArray[l];
-                l ++;
-            }
+        // 如果originRestIndex比originIndex小，则说明compareArray有比较大的元素未替换，需要补全
+        while (originResetIndex < originIndex) {
+            originArray[originResetIndex++] = compareArray[compareIndex++];
         }
     }
-
 }
