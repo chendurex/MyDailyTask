ca1951c7816a31994c64c564a72703e001115a6b
chendurex <chendurex@gmail.com>
2016-08-07 10:45:43 +0800

    heap sort

--- src/main/java/com/algorithms/HeapSort.java
+++ src/main/java/com/algorithms/HeapSort.java
@@ -9,50 +9,48 @@ import java.util.Arrays;
 /* Class HeapSort */
 public class HeapSort {
     private static int[] array = {1, 9, 18, 25, 4, 6, 2, 99, 50};
-    private static int len;
+    private static int arrayLen;
 
     public static void main(String[] args) {
-        sort(array);
+        sort();
         System.out.println(Arrays.toString(array));
     }
 
-    /* Sort Function */
-    public static void sort(int arr[]) {
-        heapify(arr);
-        for (int i = len; i > 0; i--) {
-            swap(arr, 0, i);
-            len = len - 1;
-            maxheap(arr, 0);
+    public static void sort() {
+        buildHeap();
+        for (int i = 0, len = array.length - 1; i <= len; i++) {
+            swap(0, arrayLen);
+            arrayLen--;
+            maxHeap(0);
         }
     }
 
-    /* Function to build a heap */
-    public static void heapify(int arr[]) {
-        len = arr.length - 1;
-        for (int i = len / 2; i >= 0; i--)
-            maxheap(arr, i);
+    public static void buildHeap() {
+        arrayLen = array.length - 1;
+        for (int len = array.length/2 - 1, i = len; i >= 0; i--) {
+            maxHeap(i);
+        }
     }
 
-    /* Function to swap largest element in heap */
-    public static void maxheap(int arr[], int i) {
-        int left = 2 * i;
-        int right = 2 * i + 1;
-        int max = i;
-        if (left <= len && arr[left] > arr[i])
+    public static void maxHeap(int cur) {
+        int left = 2 * cur + 1;
+        int right = 2 * cur + 2;
+        int max = cur;
+        if (left <= arrayLen && array[max] < array[left]) {
             max = left;
-        if (right <= len && arr[right] > arr[max])
+        }
+        if (right <= arrayLen && array[max] < array[right]) {
             max = right;
-
-        if (max != i) {
-            swap(arr, i, max);
-            maxheap(arr, max);
+        }
+        if (cur != max) {
+            swap(cur, max);
+            maxHeap(max);
         }
     }
 
-    public static void swap(int arr[], int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+    public static void swap(int cur, int max) {
+        int temp = array[cur];
+        array[cur] = array[max];
+        array[max] = temp;
     }
-
 }
