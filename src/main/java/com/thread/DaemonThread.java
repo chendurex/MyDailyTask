7ad4d4c91fb58520604ae3ab09973ddf7a8e9305
chendurex <chendurex@gmail.com>
2016-08-26 13:34:38 +0800

    amq demo

--- src/main/java/com/thread/DaemonThread.java
+++ src/main/java/com/thread/DaemonThread.java
@@ -1,32 +1,24 @@
 package com.thread;
 
-import org.junit.Test;
-
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by Administrator on 2016/8/20.
  */
-public class DaemonThread {
-    @Test
-    public void test() throws Exception{
-        final ReentrantLock lock = new ReentrantLock();
-        final Condition condition = lock.newCondition();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (this) {
-                    try {
-                        condition.wait(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, "my_daemon_thread").start();
+public class DaemonThread extends Thread {
 
+    public static void main(String[] args) {
+        DaemonThread threadTest  = new DaemonThread();
+        threadTest.setDaemon(true);
+        threadTest.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
