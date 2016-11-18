683c8f0eeabfa9bb42f12859715bf5c506f4b071
chendurex <chendurex@gmail.com>
2016-11-18 21:03:30 +0800

    proxy demo

--- src/main/java/com/jdk/proxy/ProxyMain.java
+++ src/main/java/com/jdk/proxy/ProxyMain.java
@@ -1,18 +1,41 @@
 package com.jdk.proxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 /**
  * @author chen
- * @description TODO
+ * @description
  * @pachage com.jdk.proxy
  * @date 2016/5/24 20:04
  */
-public class ProxyMain {
-    public static void main(String[] args) {
+public class ProxyMain extends ClassLoader {
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+
+        return super.loadClass(name);
+    }
+
+    public static void main(String[] args) throws Exception{
         ComparableTest comparableTest = new ComparableTest("test");
-        Comparable comparable = (Comparable) Proxy.newProxyInstance(ProxyMain.class.getClassLoader(),
+        Comparable comparable1 = (Comparable) Proxy.newProxyInstance(ProxyMain.class.getClassLoader(),
                 new Class[]{Comparable.class},new ProxyObject(comparableTest));
-        System.out.println(comparable.compareTo("test2"));
+        System.out.println(comparable1.compareTo("test2"));
+
+
+        Comparable comparable2 = (Comparable) Proxy.getProxyClass(ProxyMain.class.getClassLoader(), Comparable.class)
+                .getConstructor(new Class[] { InvocationHandler.class })
+                .newInstance(new Object[] {new ProxyObject(comparableTest)});
+        System.out.println(comparable2.compareTo("test2"));
+
+      //  byte[] b = ProxyGenerator.generateProxyClass("com.sun.proxy.$Proxy0", new Class[] {Comparable.class});
+       // FileOutputStream fos = new FileOutputStream(new File("d:\\$Proxy0.class"));
+        //fos.write(b);
+       // fos.close();
     }
 }
