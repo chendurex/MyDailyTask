ef53830167bba0bfe5b1e3dea384356d87590318
chendurex <chendurex@gmail.com>
2016-11-19 17:26:55 +0800

    byte code view

---
+++ src/main/java/com/jvm/bytecode/ForEachCode.java
@@ -0,0 +1,13 @@
+package com.jvm.bytecode;
+
+/**
+ * @author chen
+ * @date 2016/11/19 16:08
+ */
+public class ForEachCode {
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+
+        }
+    }
+}
---
+++ src/main/java/com/jvm/bytecode/TwoConstruct.java
@@ -0,0 +1,15 @@
+package com.jvm.bytecode;
+
+/**
+ * @author chen
+ * @date 2016/11/19 16:28
+ */
+public class TwoConstruct {
+    private String name;
+    public TwoConstruct() {
+    }
+
+    public TwoConstruct(String name) {
+        this.name = name;
+    }
+}
